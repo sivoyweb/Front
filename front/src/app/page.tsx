9603e4f045020de2ba0866112e38819d26de8b9d
Bárbara Pérez de Arce <barbara.perezdearce@gmail.com>
2024-09-19 10:05:07 -0300

    Push 1 19/09
    Header y Maqueteo Home

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,48 @@
+import Image from "next/image";
+import tudestino from "../assets/tudestino.png"
+
 const Home:React.FC = () => {
   return (
     <div>
-      <div className="text-3xl text-sivoy-green font-arialroundedmtbold">este es el home</div>
-      <div className="text-xl text-sivoy-orange font-dinroundpro">bajada del home</div>
+      <header className="mt-56 mr-10 relative overflow-hidden before: content-none min-h-[400px] w-[calc(100% - 2rem)] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[url(../assets/chalten.jpg)] bg-center bg-cover rounded-3xl z-10 p-8 md:p-16">
+        <div className="grid">
+          <div className="pt-16 pr-4">
+            <div className="relative flex justify-center">
+              <Image src={tudestino} alt="tu destino sin límites" className="absolute top-[-50px] w-3/4 h-auto"/>
+            </div>
+            <h1 className="relative flex justify-center h-auto  top-[-60px] font-arialroundedmtbold">
+              Información sobre turismo accesible
+            </h1>
+
+            <a href="/destinations" className="flex align-middle justify-center mt-24">
+              <button className="rounded-3xl bg-sivoy-orange py-2 px-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-orange-700 focus:shadow-none active:bg-orange-700 hover:bg-orange-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2">
+                Explorar Destinos
+              </button>
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <section>
+        <h2>Destinos populares</h2>
+        <p>Descubre los destinos accesibles mejor valorados de Argentina</p>
+
+        <div>
+          <div>
+            <p>Acá van las tarjetas de cada destino</p>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2>¿Quienes somos?</h2>
+      </section>
+
+      <section>
+        <h2>Servicios</h2>
+      </section>
+
+
     </div>
   )
 }
