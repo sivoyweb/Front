6f1945b3ab3ffb150d972f3d064523add0c1b63e
CrisAcevey <acevey.cris@gmail.com>
2024-09-22 22:56:39 -0300

    Termine el contexto de travels, hice las tarjetas de travel y renderize el mapeado del grid de travels, tambien hice la pagina dinamica del detalle de cada destino

---
+++ front/src/components/TravelGrid/index.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useContext } from "react";
+import { TravelContext } from "../../context/travelContext";
+import TravelCard from "../TravelCard";
+import { ITravel } from "@/interfaces/interfaces";
+
+ function TravelGridComponent() {
+  
+    const {travels} = useContext(TravelContext);
+    return (
+        <div className="flex justify-center m-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {travels.map((travels: ITravel) => (
+        <TravelCard travels ={travels} key={travels.id} />
+      ))}
+    </div>
+  </div>
+    );
+}
+
+export default TravelGridComponent;
