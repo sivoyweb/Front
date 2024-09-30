99b79bad6b205e256c22475a5448bad02af63bc3
Bárbara Pérez de Arce <barbara.perezdearce@gmail.com>
2024-09-30 11:41:29 -0300

    listo blog

--- front/src/lib/server/fetchBlog.ts
+++ front/src/lib/server/fetchBlog.ts
@@ -2,7 +2,7 @@ import { IBlogArticle } from "@/interfaces/interfaces";
 
 export async function fetchBlog(): Promise<IBlogArticle[]> {
     try {
-        const response = await fetch("https://api-sivoy.onrender.com/blog");
+        const response = await fetch("https://api-sivoy.onrender.com/blogs");
 
         if (!response.ok) {
             throw new Error(`Error al obtener los datos: ${response.status} ${response.statusText}`);
@@ -18,7 +18,7 @@ export async function fetchBlog(): Promise<IBlogArticle[]> {
 
 
 export async function fetchBlogArticleById(id: string): Promise<IBlogArticle> {
-    const response = await fetch(`https://api-sivoy.onrender.com/blog/${id}`);
+    const response = await fetch(`https://api-sivoy.onrender.com/blogs/${id}`);
     const blogArticle = await response.json();
     return blogArticle;
 };
