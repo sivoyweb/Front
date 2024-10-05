6c44385bc5de5966f54039897f0ee217910a39e0
Ian Tarquini <ianricar4@gmail.com>
2024-10-05 20:42:41 -0300

    .

--- front/src/components/UserDashboard/index.tsx
+++ front/src/components/UserDashboard/index.tsx
@@ -336,7 +336,7 @@ const UserDashboard = () => {
                   </CldUploadWidget>
                 </div>
                 <button
-                  onClick={handleSubmit}
+                  
                   type="submit"
                   disabled={isSubmitting}
                   className="mt-4 text-white px-4 py-2 hover:text-gray-700 text-align-right ml-auto block"
