f68da79a8b7e5f37fc04154e2139246ee4f9c51d
Ian Tarquini <ianricar4@gmail.com>
2024-10-05 19:06:28 -0300

    userDashboard terminado

--- front/src/components/UserDashboard/index.tsx
+++ front/src/components/UserDashboard/index.tsx
@@ -5,26 +5,52 @@ import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-//import { changeData } from "@/lib/server/fetchUsers";
 import { CldUploadWidget, CloudinaryUploadWidgetInfo } from 'next-cloudinary';
 import axios from "axios";
 
 
+interface Credential {
+  avatar: {
+    url: string;
+    publicId: string;
+  };
+}
+interface FormData {
+  name: string;
+  phone: string;
+  disabilities: string[];
+  credential: Credential;
+  isRepresentative: boolean;
+  id: string | undefined; 
+}
+
 
 const UserDashboard = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
   const { isLogged,user } = useContext(UserContext);
   const router = useRouter()
- const token = localStorage.getItem('token');
+
+
+  const disabilitiesOption=[
+    { category: 'Visual', selected: false },
+    { category: 'Auditiva', selected: false },
+    { category: 'Motora', selected: false },
+    { category: 'Fisica', selected: false },
+    { category: 'Sensorial', selected: false },
+    { category: 'Cognitiva', selected: false },
+    { category: 'Psicosocial', selected: false },
+  ]
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken);
+  }, []);
   
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     name:user?.name || '',
     phone:user?.phone || '',
-    disabilities:[
-      { category: 'Visual', name: 'Ceguera' },
-      { category: 'Auditiva', name: 'Sordera' },
-      { category: 'Motora', name: 'Parálisis' }
-    ],
+    disabilities:[],
     credential: {
       avatar: {
         url: '',
@@ -32,11 +58,14 @@ const UserDashboard = () => {
       },
     },
     id:user?.id,
+    isRepresentative:false
     
    });
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [activeSection, setActiveSection] = useState('account');
   const [isEditing, setIsEditing] = useState(false); 
+  const [selectedDisabilities, setSelectedDisabilities] = useState<string[]>([]);
+  const [isDisabilityListOpen, setIsDisabilityListOpen] = useState(false);
   
   const {data:session} = useSession();
   
@@ -57,6 +86,9 @@ const UserDashboard = () => {
   
   const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); 
+
+  
+    
     Swal.fire({
     title: '¿Estás seguro de los cambios?',
     text: "Revisa los campos antes de continuar.",
@@ -111,24 +143,44 @@ const UserDashboard = () => {
     }));
   };
 
-  // const handleChangeDisability = (e: React.ChangeEvent<HTMLInputElement>, index: number, field: string) => {
-  //   const { value } = e.target;
   
-  //   setFormData((prevData) => {
-  //     const updatedDisability = [...prevData.disabilities];
-  //     updatedDisability[index] = {
-  //       ...updatedDisability[index],
-  //       [field]: value, 
-  //     };
-  
-  //     return {
-  //       ...prevData,
-  //       disability: updatedDisability,
-  //     };
-  //   });
-  // };
 
- 
+  const handleToggleDisability = (disability: string) => {
+    setSelectedDisabilities((prev)=>{
+      if(prev.includes(disability)){
+        return prev.filter((item) => item !== disability);
+      } else {
+        return [...prev, disability];
+      }
+    });
+    setFormData((prev: FormData) => {
+      const { disabilities } = prev;
+  
+      
+      const newDisabilities = disabilities.includes(disability)
+        ? disabilities.filter(d => d !== disability) 
+        : [...disabilities, disability]; 
+       
+  
+      return {
+        ...prev,
+        disabilities: newDisabilities,
+      };
+    });
+    
+  };
+
+  const handleRepresentativeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      isRepresentative: checked, 
+    }));
+  };
+  
+
+  const toggleDisabilityList = () => setIsDisabilityListOpen(!isDisabilityListOpen);
+
 
 
 
@@ -136,16 +188,7 @@ const UserDashboard = () => {
     
     switch (activeSection) {
      
-      case 'favorites':
-        return (
-          <div className="bg-white rounded-lg shadow-lg p-6">
-            <h2 className="text-xl font-semibold mb-4">Lugares Favoritos</h2>
-            <ul>
-              <li className="text-gray-600">Lugar 1: ------</li>
-              <li className="text-gray-600">Lugar 2: ------</li>
-            </ul>
-          </div>
-        );
+      
       case 'account':
         return (
           <div className="bg-white rounded-lg shadow-lg p-6 mb-20">
@@ -163,6 +206,7 @@ const UserDashboard = () => {
                 <p className="text-gray-600">Nombre: {user?.name || session?.user?.name}</p>
                 <p className="text-gray-600">Email: {user?.credential?.email || session?.user?.email}</p>
                {!session?.user ? <p className="text-gray-600">Teléfono:{user?.phone} </p> : null}
+               <p className="text-gray-600">Representante: {formData.isRepresentative ? "Sí" : "No"}</p>
                 
               </>
             ) : (
@@ -191,90 +235,69 @@ const UserDashboard = () => {
                     placeholder="Ingresa tu número de teléfono"
                   />
                 </div>
+                <div>
+                  <label className="flex items-center">
+                    <input
+                      type="checkbox"
+                      name="isRepresentative"
+                      checked={formData.isRepresentative}
+                      onChange={handleRepresentativeChange} 
+                      className="mr-2"
+                    />
+                    Soy representante de alguien más
+                  </label>
+                </div>
                 <div className="flex gap-4">
   {/*categories*/}
   <div className="w-full max-w-xs flex flex-col gap-1">
-    <label htmlFor="disabilities" className="w-fit pl-0.5 text-sm text-neutral-600 dark:text-neutral-300">Disabilities</label>
+    <label htmlFor="disabilities" className="w-fit pl-0.5 text-sm text-neutral-600 dark:text-neutral-300">Discapacidad</label>
     <div className="relative">
-      <button type="button" role="combobox" className="inline-flex w-full items-center justify-between gap-2 whitespace-nowrap border-neutral-300 bg-neutral-50 px-4 py-2 text-sm font-medium capitalize tracking-wide text-neutral-600 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black dark:border-neutral-700 dark:bg-neutral-900/50 dark:text-neutral-300 dark:focus-visible:outline-white border rounded-md" aria-haspopup="listbox" aria-controls="disabilitiesList">
-        <span className="text-sm w-full font-normal text-start overflow-hidden text-ellipsis  whitespace-nowrap">Please Select</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-          <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"/>
-        </svg>
-      </button>
+    <button
+  type="button"
+  role="combobox"
+  onClick={toggleDisabilityList}
+  className="inline-flex w-full items-center justify-between gap-2 whitespace-nowrap border-0 bg-transparent px-4 py-2 text-sm font-medium capitalize tracking-wide text-neutral-600 transition hover:bg-gray-200 focus:bg-gray-300 focus:outline-none"
+  aria-haspopup="listbox"
+  aria-controls="namesList"
+>
+  <span className="text-sm w-full font-normal text-start overflow-hidden text-ellipsis whitespace-nowrap">
+    {selectedDisabilities.length > 0 ? selectedDisabilities.join(', ') : "Seleccione una opción"}
+  </span>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+    <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"/>
+  </svg>
+</button>
 
-      <input id="disabilities" name="disabilities" type="text" hidden />
-      <ul id="disabilitiesList" className="absolute z-10 left-0 top-11 flex max-h-44 w-full flex-col overflow-hidden overflow-y-auto border-neutral-300 bg-neutral-50 py-1.5 dark:border-neutral-700 dark:bg-neutral-900 border rounded-md" role="listbox">
-        <li role="option">
-          <label className="flex cursor-pointer items-center gap-2 px-4 py-3 text-sm font-medium text-neutral-600 hover:bg-neutral-950/5 dark:text-neutral-300 dark:hover:bg-white/5">
-            <div className="relative flex items-center">
-              <input type="checkbox" className="combobox-option peer relative size-4 cursor-pointer appearance-none overflow-hidden border border-neutral-300 bg-neutral-50 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-neutral-800 rounded dark:border-neutral-700 dark:bg-neutral-900" />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeWidth="4" className="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-neutral-100 peer-checked:visible dark:text-black" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-              </svg>
-            </div>
-            <span>Disability Option 1</span>
-          </label>
-        </li>
-        <li role="option">
-          <label className="flex cursor-pointer items-center gap-2 px-4 py-3 text-sm font-medium text-neutral-600 hover:bg-neutral-950/5 dark:text-neutral-300 dark:hover:bg-white/5">
-            <div className="relative flex items-center">
-              <input type="checkbox" className="combobox-option peer relative size-4 cursor-pointer appearance-none overflow-hidden border border-neutral-300 bg-neutral-50 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-neutral-800 rounded dark:border-neutral-700 dark:bg-neutral-900" />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeWidth="4" className="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-neutral-100 peer-checked:visible dark:text-black" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-              </svg>
-            </div>
-            <span>Disability Option 2</span>
-          </label>
-        </li>
+      <input id="disabilities" name="disabilities" type="text"  hidden value={selectedDisabilities.join(',')} />
+      <ul id="disabilitiesList"
+      className={`absolute z-10 left-0 top-11 flex max-h-44 w-full flex-col overflow-hidden overflow-y-auto border-neutral-300 bg-neutral-50 py-1.5 dark:border-neutral-700 dark:bg-neutral-900 border rounded-md transition-height ${
+        isDisabilityListOpen ? 'visible-list' : 'hidden-list'
+      }`} role="listbox">
+
+        {disabilitiesOption.map(option => (
+              <li key={option.category} role="option">
+                <label className="combobox-label">
+                  <input
+                    type="checkbox"
+                    checked={formData.disabilities.includes(option.category)}
+                    onChange={() => handleToggleDisability(option.category)}
+                  />
+                  <span>{option.category}</span>
+                </label>
+              </li>
+            ))}
       </ul>
     </div>
   </div>
 
-  {/*names*/}
-  <div className="w-full max-w-xs flex flex-col gap-1">
-    <label htmlFor="names" className="w-fit pl-0.5 text-sm text-neutral-600 dark:text-neutral-300">Names</label>
-    <div className="relative">
-      <button type="button" role="combobox" className="inline-flex w-full items-center justify-between gap-2 whitespace-nowrap border-neutral-300 bg-neutral-50 px-4 py-2 text-sm font-medium capitalize tracking-wide text-neutral-600 transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black dark:border-neutral-700 dark:bg-neutral-900/50 dark:text-neutral-300 dark:focus-visible:outline-white border rounded-md" aria-haspopup="listbox" aria-controls="namesList">
-        <span className="text-sm w-full font-normal text-start overflow-hidden text-ellipsis  whitespace-nowrap">Please Select</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-          <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"/>
-        </svg>
-      </button>
-
-      <input id="names" name="names" type="text" hidden />
-      <ul id="namesList" className="absolute z-10 left-0 top-11 flex max-h-44 w-full flex-col overflow-hidden overflow-y-auto border-neutral-300 bg-neutral-50 py-1.5 dark:border-neutral-700 dark:bg-neutral-900 border rounded-md" role="listbox">
-        <li role="option">
-          <label className="flex cursor-pointer items-center gap-2 px-4 py-3 text-sm font-medium text-neutral-600 hover:bg-neutral-950/5 dark:text-neutral-300 dark:hover:bg-white/5">
-            <div className="relative flex items-center">
-              <input type="checkbox" className="combobox-option peer relative size-4 cursor-pointer appearance-none overflow-hidden border border-neutral-300 bg-neutral-50 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-neutral-800 rounded dark:border-neutral-700 dark:bg-neutral-900" />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeWidth="4" className="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-neutral-100 peer-checked:visible dark:text-black" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-              </svg>
-            </div>
-            <span>Nombre 1</span>
-          </label>
-        </li>
-        <li role="option">
-          <label className="flex cursor-pointer items-center gap-2 px-4 py-3 text-sm font-medium text-neutral-600 hover:bg-neutral-950/5 dark:text-neutral-300 dark:hover:bg-white/5">
-            <div className="relative flex items-center">
-              <input type="checkbox" className="combobox-option peer relative size-4 cursor-pointer appearance-none overflow-hidden border border-neutral-300 bg-neutral-50 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-neutral-800 rounded dark:border-neutral-700 dark:bg-neutral-900" />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeWidth="4" className="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-neutral-100 peer-checked:visible dark:text-black" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-              </svg>
-            </div>
-            <span>Nombre 2</span>
-          </label>
-        </li>
-      </ul>
-    </div>
-  </div>
+  
+ 
 </div>
 
 
 
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">Avatar</label>
+                  <label className="block text-sm font-medium text-gray-700 mt-12 ">Avatar</label>
                   <CldUploadWidget uploadPreset="siVoyPreset"
                                    onSuccess={(result)=>{
 
@@ -347,15 +370,7 @@ const UserDashboard = () => {
         </div>
         <nav className="mt-10">
          
-          <a
-            href="#"
-            onClick={() => setActiveSection('favorites')}
-            className={`block py-2.5 px-4 rounded transition-all duration-200 hover:bg-sivoy-blue ${
-              !sidebarOpen ? "opacity-0 w-0" : "opacity-100 w-full"
-            }`}
-          >
-            Favoritos
-          </a>
+         
           <a
             href="#"
             onClick={() => setActiveSection('account')}
